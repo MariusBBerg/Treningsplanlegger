29b3a51627296620213abfa0818f7d48df693d56
Marius Bakken Berg <marius.bakken.berg@gmail.com>
2024-02-29 19:23:08 +0100

    Register endepunkt basic

--- treningsplanlegging/src/main/java/com/treningsplanlegging/treningsplanlegging/config/SecurityConfig.java
+++ treningsplanlegging/src/main/java/com/treningsplanlegging/treningsplanlegging/config/SecurityConfig.java
@@ -5,12 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            // Deaktiver CSRF
+            .csrf().disable()
+            // Tillat alle forespørsler til alle URLer
+            .authorizeRequests()
+                .anyRequest().permitAll()
+            // Ytterligere konfigurasjoner kan legges til her
+            ;
+
+        return http.build();
+    }
 }
+
 
